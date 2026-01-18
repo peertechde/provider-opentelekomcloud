a53ebdf3bad1ce2204446a43f345a031c083a40c
Marcel Kirschner <marcel.kirschner@peertech.de>
2026-01-18 10:43:12 +0100

    chore: update VPC status documentation and register all controllers

--- internal/controller/opentelekomcloud.go
+++ internal/controller/opentelekomcloud.go
@@ -5,6 +5,10 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/peertechde/provider-opentelekomcloud/internal/controller/config"
+	"github.com/peertechde/provider-opentelekomcloud/internal/controller/securitygroup"
+	"github.com/peertechde/provider-opentelekomcloud/internal/controller/securitygrouprule"
+	"github.com/peertechde/provider-opentelekomcloud/internal/controller/subnet"
+	"github.com/peertechde/provider-opentelekomcloud/internal/controller/vpc"
 )
 
 // SetupGated creates all OpenTelekomCloud controllers with safe-start support and adds them to
@@ -12,6 +16,10 @@ import (
 func SetupGated(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
 		config.Setup,
+		vpc.SetupGated,
+		subnet.SetupGated,
+		securitygroup.SetupGated,
+		securitygrouprule.SetupGated,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
